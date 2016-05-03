8c6a46465c6383aac30da196afc3d75fbdf684fc
Evan Rubinton <evanrubinton@gmail.com>
2016-05-02 20:41:11 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
+      }
+    ]
+  },
+  postcss: function() {
+    return [require('autoprefixer'), require('precss')];
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
