c98676324b0d6a8260520480b7f66d712e0fa9b1
Evan Rubinton <evanrubinton@gmail.com>
2016-05-06 19:59:22 -0400

    consolidate leftdrawer and nav components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {List} from 'immutable';
 import Nav from './Nav';
-import LeftDrawer from './LeftDrawer';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
@@ -11,29 +10,13 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 //Discussion - open forum discussion
 const navItems = List.of('Home', 'Upcoming', 'Sales', 'Music', 'Discussion');
 const appTitle = "GamerLyfe";
-var leftDrawerOpen = false;
-
-function toggleLeftDrawer(){
-  leftDrawerOpen = !leftDrawerOpen;
-};
 
 export default React.createClass({
-  getInitialState: function(){
-    return {
-      leftDrawerOpen: false
-    };
-  },
-
-  toggleLeftDrawer: function(){
-    this.setState({leftDrawerOpen: !this.state.leftDrawerOpen});
-  },
-
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
         <div>
-          <Nav appTitle={appTitle} leftIconCB={this.toggleLeftDrawer} />
-          <LeftDrawer open={this.state.leftDrawerOpen} navItems={navItems}/>
+          <Nav navItems={navItems} appTitle={appTitle} leftIconCB={this.toggleLeftDrawer} />
           {this.props.children}
           </div>
       </MuiThemeProvider>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import { Link } from 'react-router';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+//TODO: Add on click handler to body/document that emits redux action to close this drawer
+
 
 export default React.createClass({
   mixins: [PureRenderMixin],
+  getInitialState: function(){
+    return {
+      leftDrawerOpen: false
+    };
+  },
+
+  getNavItems: function(){
+    return this.props.navItems || [];
+  },
+
+  toggleLeftDrawer: function(){
+    this.setState({leftDrawerOpen: !this.state.leftDrawerOpen});
+  },
+
   render: function() {
     return (
-      <AppBar
-        title={<Link to="/home">{this.props.appTitle}</Link>}
-        onLeftIconButtonTouchTap={this.props.leftIconCB}
-      />
+      <div>
+        <AppBar
+          title={<Link to="/home">{this.props.appTitle}</Link>}
+          onLeftIconButtonTouchTap={this.toggleLeftDrawer} />
+        <Drawer open={this.state.leftDrawerOpen}>
+          {this.getNavItems().map((item) => {
+            return <MenuItem key={item}>{item}</MenuItem>;
+          })}
+          </Drawer>
+      </div>
     );
   }
 });
