e4022f98fbd73ae0bd959f093ef9cae1cdf5f7e1
Evan Rubinton <evanrubinton@gmail.com>
2016-05-07 11:45:15 -0400

    add handle-clickoutside module, use in nav cmp

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,26 +2,29 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-
-import * as actionCreators from '../action_creators';
-
 import { Link } from 'react-router';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux';
 
+import * as actionCreators from '../action_creators';
+import onClickOutside from 'react-onclickoutside';
+
 //Home - where users can view currently playing, their own collection, etc
 //Upcoming - where users can see a list of upcoming games with links to each game's page/discussions
 // TODO: Render recently viewed list of games underneath here in nav
-// TODO: Add on click handler to body/document that emits redux action to close this drawer
 
-const Nav = React.createClass({
+const Nav =  onClickOutside(React.createClass({
   mixins: [PureRenderMixin],
+
+  handleClickOutside: function(e) {
+    this.props.toggleLeftDrawer();
+  },
 
   getDrawerItems: function(){
     return this.props.leftDrawerItems || [];
   },
 
-  render: function() {
+  render() {
     return (
       <div className="nav-ctr">
         <AppBar
@@ -32,10 +35,10 @@ const Nav = React.createClass({
             return <MenuItem style={{color: 'black'}}  key={item}>{item}</MenuItem>;
           })}
           </Drawer>
-      </div>
+        </div>
     );
   }
-});
+}));
 
 function mapStateToProps(state){
   return {
