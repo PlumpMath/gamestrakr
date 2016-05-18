0379e1a6ba0b8d9ce71491d24748764bf312434a
Evan Rubinton <evanrubinton@gmail.com>
2016-05-18 15:43:16 -0400

    add popover menu to game tiles

--- src/components/games/Tile.jsx
+++ src/components/games/Tile.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {GridTile} from 'material-ui/GridList';
+import FontIcon from 'material-ui/FontIcon';
+import IconButton from 'material-ui/IconButton';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Popover from 'material-ui/Popover';
 
 const placeholderImageUrl = 'https://placeholdit.imgix.net/~text?txtsize=38&txt=GamerLyfe&w=450&h=300&txttrack=0'
 
@@ -9,7 +14,8 @@ const Tile = React.createClass({
 
   getInitialState: function(){
     return {
-      imageUrl: this.getGameImageUrl(this.props.item)
+      imageUrl: this.getGameImageUrl(this.props.item),
+			popOverOpen: false
     };
   },
 
@@ -21,15 +27,40 @@ const Tile = React.createClass({
     this.setState({imageUrl: placeholderImageUrl});
   },
 
+	onPlusTap: function(e){
+		e.preventDefault();
+		this.setState({popOverOpen: true, popOverAnchor: e.currentTarget});
+	},
+
+	onClosePopOver: function(){
+		this.setState({popOverOpen: false});
+	},
+
   render(){
     const {item} = this.props;
 
     return (
       <GridTile
-        title={item.get('name')}>
+        title={item.get('name')}
+        actionIcon={
+					<IconButton onTouchTap={this.onPlusTap}>
+						<FontIcon className="material-icons">add_circle</FontIcon>
+					</IconButton>}>
         <img
           src={this.state.imageUrl}
           onError={this.onImageError}/>
+				<Popover
+					open={this.state.popOverOpen}
+					anchorEl={this.state.popOverAnchor}
+					onRequestClose={this.onClosePopOver}>
+					<Menu>
+						<MenuItem primaryText="Playing" />
+						<MenuItem primaryText="Planning" />
+						<MenuItem primaryText="Completed" />
+						<MenuItem primaryText="On-Hold" />
+						<MenuItem primaryText="Dropped" />
+					</Menu>
+				</Popover>
       </GridTile>
     );
   }
