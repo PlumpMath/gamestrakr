ab3df0cc64b42223217273e4e498e37abbd19346
Evan Rubinton <evanrubinton@gmail.com>
2016-05-23 23:15:13 -0400

    add placeholder text to game detail cmp

--- src/components/games/Detail.jsx
+++ src/components/games/Detail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {hashHistory} from 'react-router';
+import {connect} from 'react-redux';
 
 import Dialog from 'material-ui/Dialog';
 
@@ -22,14 +23,23 @@ const Detail = React.createClass({
 		hashHistory.push(url);
   },
 
+  // componentDidMount: function(){
+  //   const game = this.props.location.pathname.split('/')[2];
+  //   this.fetchGameInfo();
+  // },
+
+  // fetchGameInfo: function(){
+
+  // },
+
   render(){
     return (
       <Dialog
         className="games-detail-ctr"
-        title="Dialog With Custom Width"
+        title="Game Detail Dialog"
         onRequestClose={this.navigateBack}
         open={true}>
-          This dialog spans the entire width of the screen.
+          Trust fund listicle tumblr forage poutine slow-carb. Ramps chia four dollar toast, franzen cardigan swag mixtape farm-to-table heirloom hammock single-origin coffee put a bird on it. Tote bag occupy selfies put a bird on it food truck. Meggings gastropub helvetica pour-over lo-fi, asymmetrical locavore pinterest. Echo park deep v PBR&B, etsy marfa vegan meh cold-pressed semiotics seitan banh mi fingerstache irony tattooed tacos. XOXO raw denim small batch post-ironic, bespoke everyday carry fashion axe normcore marfa waistcoat green juice hashtag street art. Shoreditch chambray kale chips microdosing godard ramps, quinoa thundercats church-key yr.
       </Dialog>
     );
   }
