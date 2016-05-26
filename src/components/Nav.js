f6b34383299b605962ce60fa8d73235eb882b5fb
Evan Rubinton <evanrubinton@gmail.com>
2016-05-26 16:23:03 -0400

    make nav a non connected component, move props to app container

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,105 +1,66 @@
-import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { Link, hashHistory } from 'react-router';
-import {connect} from 'react-redux';
-
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import FontIcon from 'material-ui/FontIcon';
-import IconMenu from 'material-ui/IconMenu';
-import IconButton from 'material-ui/IconButton';
-import MenuItem from 'material-ui/MenuItem';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-
-import {appActions, userActions} from '../actions/';
+import React, { Component, PropTypes } from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { Link } from 'react-router'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import FontIcon from 'material-ui/FontIcon'
+import IconMenu from 'material-ui/IconMenu'
+import IconButton from 'material-ui/IconButton'
+import MenuItem from 'material-ui/MenuItem'
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar'
 
 // Home - where users can view currently playing, their own collection, recently viewed etc
 // Games - where users can search for and sort through upcoming and recently released games
 // Platforms - where users can look through platforms
 
-const navLinks =  [{name: 'All Games', route: 'games', icon: 'videogame_asset'}, {name: 'My Games', route: 'my_games', icon: 'videogame_asset'}];
+const navLinks =  [{name: 'All Games', route: 'games', icon: 'videogame_asset'}, {name: 'My Games', route: 'my_games', icon: 'videogame_asset'}]
 
-const Nav =  React.createClass({
-	mixins: [PureRenderMixin],
+export default class Nav extends Component {
+  constructor(props) {
+    super(props)
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+  }
 
-  componentDidMount: function() {
-    window.addEventListener('resize', this.handleResize);
-    this.props.windowResized(window.innerWidth);
-  },
-
-  componentWillUnmount: function() {
-    window.removeEventListener('resize', this.handleResize);
-  },
-
-  handleResize: function(e) {
-    this.props.windowResized(window.innerWidth);
-  },
-
-	triggerRoute(route){
-		hashHistory.push(route);
-		this.props.closeLeftDrawer();
-	},
-
-	render() {
-    const {leftDrawerOpen, navTitle, user} = this.props;
-    const signInButton = (<MenuItem primaryText='Sign In' onTouchTap={this.triggerRoute.bind(this, '/sign_in')}/>);
-    const signOutButton= (<MenuItem primaryText='Sign Out' onTouchTap={this.props.signOut}/>);
+  render() {
+    const {leftDrawerOpen, closeLeftDrawer, openLeftDrawer, navTitle, user, triggerRoute, signOut} = this.props
+    const signInButton = <MenuItem primaryText='Sign In' onTouchTap={() => triggerRoute('/sign_in')}/>
+    const signOutButton= <MenuItem primaryText='Sign Out' onTouchTap={signOut}/>
 
     const accountNavElement = (
       <IconMenu iconButtonElement={
         <IconButton>
-          <FontIcon className="material-icons">account_circle</FontIcon>
+        <FontIcon className="material-icons">account_circle</FontIcon>
         </IconButton>}>
         {user.get('token') ? signOutButton : signInButton}
-      </IconMenu>
-    );
+        </IconMenu>
+    )
 
     return(
       <div className="nav-ctr">
-				<AppBar onLeftIconButtonTouchTap={this.props.openLeftDrawer} title={navTitle || ''} iconElementRight={accountNavElement}/>
-        <Drawer open={leftDrawerOpen}>
-          <AppBar
-            title={<Link to="home" style={{textDecoration: 'none', color: '#fff'}}>{this.props.appTitle}</Link>}
-            iconElementLeft={<IconButton onClick={this.props.closeLeftDrawer}><FontIcon className="material-icons">arrow_back</FontIcon></IconButton>}/>
-            {navLinks.map((item) => {
-              return (
-                <MenuItem key={item.name} onTouchTap={this.triggerRoute.bind(this, item.route)}>
-                  {item.name}
-                </MenuItem>
-              );
-            })}
+      <AppBar
+      onLeftIconButtonTouchTap={this.props.openLeftDrawer}
+      title={navTitle || ''}
+      iconElementRight={accountNavElement}/>
+
+      <Drawer open={leftDrawerOpen}>
+      <AppBar
+      title={<Link to="home" style={{textDecoration: 'none', color: '#fff'}}>GamesTrakr</Link>}
+      iconElementLeft={
+        <IconButton
+        onClick={closeLeftDrawer}>
+        <FontIcon className="material-icons">arrow_back</FontIcon>
+        </IconButton>}/>
+
+        {navLinks.map((item) => {
+          return (
+            <MenuItem key={item.name} onTouchTap={() => triggerRoute(item.route)}>
+            {item.name}
+            </MenuItem>
+          )
+        })}
         </Drawer>
-      </div>
-		);
-	}
-});
-
-function mapStateToProps(state){
-  return {
-    leftDrawerOpen: state.getIn(['app', 'leftDrawerOpen']),
-    navTitle: state.getIn(['app', 'currentPage']),
-    user: state.get('user')
-  };
+        </div>
+    )
+  }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    openLeftDrawer: () => {
-      dispatch(appActions.openLeftDrawer());
-    },
-    closeLeftDrawer: () => {
-      dispatch(appActions.closeLeftDrawer());
-    },
-    windowResized: (width) => {
-      dispatch(appActions.windowResized(width));
-    },
-    signOut: () => {
-      dispatch(userActions.signOut());
-    }
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Nav);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import Nav from '../components/Nav';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-const darkMuiTheme = getMuiTheme(darkBaseTheme);
+import React, { Component, PropTypes } from 'react'
+import Nav from '../components/Nav'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import {connect} from 'react-redux'
+import {appActions, userActions} from '../actions/'
+import { Link, hashHistory } from 'react-router'
 
-export default React.createClass({
+const darkMuiTheme = getMuiTheme(darkBaseTheme)
+class App extends Component {
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize)
+    this.props.windowResized(window.innerWidth)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize)
+  }
+
+  handleResize(e) {
+    this.props.windowResized(window.innerWidth)
+  }
+
+  triggerRoute(route) {
+    hashHistory.push(route)
+    this.props.closeLeftDrawer()
+  }
+
   render() {
     return (
       <MuiThemeProvider muiTheme={darkMuiTheme}>
-        <div className="app-ctr">
-          <Nav/>
-          {this.props.children}
-        </div>
+      <div className="app-ctr">
+      <Nav {...this.props} triggerRoute={this.triggerRoute} />
+      {this.props.children}
+      </div>
       </MuiThemeProvider>
-    );
+    )
   }
-});
+}
+
+function mapStateToProps(state){
+  return {
+    leftDrawerOpen: state.getIn(['app', 'leftDrawerOpen']),
+    navTitle: state.getIn(['app', 'currentPage']),
+    user: state.get('user')
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    openLeftDrawer: () => {
+      dispatch(appActions.openLeftDrawer())
+    },
+    closeLeftDrawer: () => {
+      dispatch(appActions.closeLeftDrawer())
+    },
+    windowResized: (width) => {
+      dispatch(appActions.windowResized(width))
+    },
+    signOut: () => {
+      dispatch(userActions.signOut())
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
