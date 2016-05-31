bc5db01b6afab9720d41e5a28cd73543da773785
Evan Rubinton <evanrubinton@gmail.com>
2016-05-31 12:44:44 -0400

    add uglify plugin to webpack prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -36,6 +36,9 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({'process.env.SERVER_URL': JSON.stringify('https://gamestrakr-server.herokuapp.com')}),
-    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')})
+    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')}),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false
+    })
   ]
 };
